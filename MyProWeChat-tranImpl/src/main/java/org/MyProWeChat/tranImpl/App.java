d02456d53633850f1a9d426ee104bd7dc839aaed
shenpengliang <shenpengliang@sunline.cn>
2020-06-12 14:16:55 +0800

    提交目录结构

---
+++ MyProWeChat-tranImpl/src/main/java/org/MyProWeChat/tranImpl/App.java
@@ -0,0 +1,13 @@
+package org.MyProWeChat.tranImpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
